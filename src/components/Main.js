5a0ecb676efef810f20b368702aed018225c7352
samantha frangi <sefrangi13@gmail.com>
2022-01-08 17:11:57 -0500

    wip: creates and tests index page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Index from '../pages/Index'
 import Show from '../pages/Show'
 
 
 function Main(props) {
+  const [people, setPeople] = useState(null)
+
+  const URL = "https://backend-samantha.herokuapp.com/people"
+  // const URL = "http://localhost:3001/people/"
+
+  // retrieve all the people
+  const getPeople = async () => {
+    const response = await fetch(URL)
+    const data = await response.json()
+    setPeople(data)
+  }
+
+  // run getPeople once when component is mounted
+  useEffect(() => getPeople(), [])
+
   return (
     <main>
       <Switch>
         <Route exact path='/'>
-          <Index />
+          <Index people={people} />
         </Route>
         <Route path="/people/:id" render={(rp) => <Show {...rp}/>}/>
       </Switch>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+
 function Index(props) {
-  return <h1>Index Name</h1>
+	const loaded = () => {
+		return props.people.map((person) => (
+			<div key={person._id} className='person'>
+				<Link to={`/people/${person._id}`}>
+					<h1>{person.name}</h1>
+				</Link>
+				<img src={person.image} alt={person.name} />
+				<h3>{person.title}</h3>
+			</div>
+		));
+	};
+
+    const loading = () => {
+        return <h1>Loading</h1>
+    }
+
+    return props.people ? loaded() : loading()
 }
 
 export default Index;
